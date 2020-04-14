568ab206bd55ab6d7b749da61524f11b0eb7da90
Raphael Advincula <raphaelhaf@gmail.com>
2020-04-14 08:26:35 -0300

    change project logo

--- frontend/src/pages/login/logo/logo.js
+++ frontend/src/pages/login/logo/logo.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Logo = () => (
   <div className="row justify-content-center align-items-center">
-    <img src="/images/logo.svg" style="width:150px;height:32px;" />
+    <img src="/images/logo.svg />
   </div>
 );
